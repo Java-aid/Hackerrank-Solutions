5893de54e8a55f64ea63ba3c4dd6bf8d553ebcaf
Kanahaiya Gupta <kanhaiyagupta20@gmail.com>
2017-12-10 00:12:03 +0530

    Added more solutions

---
+++ HackerRankDashboard/CoreCS/Algorithms/src/main/java/com/hackerrank/solutions/algorithms/sorting/InsertionSortPart1.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package com.hackerrank.solutions.algorithms.sorting;
+
+import java.util.Scanner;
+
+/**
+ * @author Kanahaiya Gupta
+ *
+ */
+public class InsertionSortPart1 {
+	public static void insertIntoSorted(int[] ar) {
+		int length = ar.length;
+		int elementNeedToBeInserted = ar[length - 1];
+		for (int i = length - 2; i >= 0; i--) {
+			if (ar[i] > elementNeedToBeInserted) {
+				ar[i + 1] = ar[i];
+				printArray(ar);
+
+			} else {
+				ar[i + 1] = elementNeedToBeInserted;
+				printArray(ar);
+				break;
+			}
+			if ((i == 0) && (ar[i] > elementNeedToBeInserted)) {
+				ar[i] = elementNeedToBeInserted;
+				printArray(ar);
+			}
+		}
+
+	}
+
+	/* Tail starts here */
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int s = in.nextInt();
+		int[] ar = new int[s];
+		for (int i = 0; i < s; i++) {
+			ar[i] = in.nextInt();
+		}
+		insertIntoSorted(ar);
+	}
+
+	private static void printArray(int[] ar) {
+		for (int n : ar) {
+			System.out.print(n + " ");
+		}
+		System.out.println("");
+	}
+}
