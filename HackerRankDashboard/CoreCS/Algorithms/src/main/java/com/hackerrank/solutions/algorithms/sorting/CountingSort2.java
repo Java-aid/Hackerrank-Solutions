5e8ff9cee3c4be51012a8a32e7e966789cfc6e44
Kanahaiya Gupta <kanhaiyagupta20@gmail.com>
2017-12-19 23:03:46 +0530

    Added more solution in Languages-->java-->advanced section

---
+++ HackerRankDashboard/CoreCS/Algorithms/src/main/java/com/hackerrank/solutions/algorithms/sorting/CountingSort2.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.hackerrank.solutions.algorithms.sorting;
+
+import java.util.Scanner;
+
+/**
+ * @author Kanahaiya Gupta
+ *
+ */
+public class CountingSort2 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int a[] = new int[N];
+		int b[] = new int[100];
+		int c[] = new int[N + 1];
+
+		for (int i = 0; i < N; i++) {
+			a[i] = sc.nextInt();
+		}
+
+		for (int i = 0; i < N; i++) {
+			b[a[i]]++;
+		}
+
+		for (int i = 1; i < 100; i++) {
+			b[i] = b[i] + b[i - 1];
+		}
+
+		for (int j = 0; j < N; j++) {
+			c[b[a[j]]] = a[j];
+			b[a[j]] = b[a[j]] - 1;
+		}
+
+		for (int j = 1; j <= N; j++) {
+			System.out.print(c[j] + " ");
+		}
+
+	}
+
+}
---
+++ HackerRankDashboard/CoreCS/Algorithms/src/main/java/com/hackerrank/solutions/algorithms/sorting/CountingSort3.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.hackerrank.solutions.algorithms.sorting;
+
+import java.util.Scanner;
+
+/**
+ * @author Kanahaiya Gupta
+ *
+ */
+public class CountingSort3 {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int a[] = new int[N];
+		int b[] = new int[100];
+
+		for (int i = 0; i < N; i++) {
+			a[i] = sc.nextInt();
+			sc.next();
+		}
+
+		for (int i = 0; i < N; i++) {
+			b[a[i]]++;
+		}
+
+		for (int i = 1; i < 100; i++) {
+			b[i] = b[i] + b[i - 1];
+		}
+
+		for (int j = 0; j < 100; j++) {
+			System.out.print(b[j] + " ");
+		}
+
+	}
+
+}
