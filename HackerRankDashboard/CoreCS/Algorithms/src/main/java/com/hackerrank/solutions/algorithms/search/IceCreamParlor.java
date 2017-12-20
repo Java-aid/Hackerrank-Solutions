ad0bbf598b1d2cf0acefd398a9f840b14efe53ec
Kanahaiya Gupta <kanhaiyagupta20@gmail.com>
2017-12-21 01:10:17 +0530

    Added more solutions inside CoreCS-->Algorithms-->search section

---
+++ HackerRankDashboard/CoreCS/Algorithms/src/main/java/com/hackerrank/solutions/algorithms/search/IceCreamParlor.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package com.hackerrank.solutions.algorithms.search;
+
+import java.util.Scanner;
+
+/**
+ * @author Kanahaiya Gupta
+ *
+ */
+public class IceCreamParlor {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for (int i = 0; i < t; i++) {
+			int m = sc.nextInt();
+			int n = sc.nextInt();
+			int a[] = new int[n];
+			for (int j = 0; j < n; j++) {
+				a[j] = sc.nextInt();
+			}
+
+			for (int j = 0; j < n; j++) {
+				for (int k = j + 1; k < n; k++) {
+					if (m == a[j] + a[k]) {
+						if (j > k) {
+							System.out.println(++k + " " + ++j);
+						} else {
+							System.out.println(++j + " " + ++k);
+						}
+						break;
+					}
+
+				}
+			}
+		}
+	}
+}
---
+++ HackerRankDashboard/CoreCS/Algorithms/src/main/java/com/hackerrank/solutions/algorithms/search/LonelyInteger.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.hackerrank.solutions.algorithms.search;
+
+import java.util.Scanner;
+
+/**
+ * @author Kanahaiya Gupta
+ *
+ */
+public class LonelyInteger {
+	static int lonelyinteger(int[] a) {
+		int result = 0;
+		for (int i = 0; i < a.length; i++) {
+			result ^= a[i];
+
+		}
+
+		return result;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int res;
+
+		int _a_size = Integer.parseInt(in.nextLine());
+		int[] _a = new int[_a_size];
+		int _a_item;
+		String next = in.nextLine();
+		String[] next_split = next.split(" ");
+
+		for (int _a_i = 0; _a_i < _a_size; _a_i++) {
+			_a_item = Integer.parseInt(next_split[_a_i]);
+			_a[_a_i] = _a_item;
+		}
+
+		res = lonelyinteger(_a);
+		System.out.println(res);
+
+	}
+}
