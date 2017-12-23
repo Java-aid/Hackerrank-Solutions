f7146640001c50320517e9570c1311e63504d3d7
Kanahaiya Gupta <kanhaiyagupta20@gmail.com>
2017-12-23 17:00:06 +0530

    Added more solutions inside CoreCS-->Algorithms-->strings section

---
+++ HackerRankDashboard/CoreCS/Algorithms/src/main/java/com/hackerrank/solutions/algorithms/strings/CamelCase.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.hackerrank.solutions.algorithms.strings;
+
+import java.util.Scanner;
+
+/**
+ * @author Kanahaiya Gupta
+ *
+ */
+public class CamelCase {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int count = 1;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) >= 65 && s.charAt(i) <= 90) {
+				count++;
+			}
+		}
+		System.out.println(count);
+
+	}
+}
---
+++ HackerRankDashboard/CoreCS/Algorithms/src/main/java/com/hackerrank/solutions/algorithms/strings/MarsExploration.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.hackerrank.solutions.algorithms.strings;
+
+import java.util.Scanner;
+
+/**
+ * @author Kanahaiya Gupta
+ *
+ */
+public class MarsExploration {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int l = s.length();
+		String tmp = "SOS";
+		int n = l / 3;
+		String newString = tmp;
+		for (int i = 1; i < n; i++) {
+			newString += tmp;
+		}
+		int count = 0;
+		for (int i = 0; i < l; i++) {
+			if (newString.charAt(i) != s.charAt(i)) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
