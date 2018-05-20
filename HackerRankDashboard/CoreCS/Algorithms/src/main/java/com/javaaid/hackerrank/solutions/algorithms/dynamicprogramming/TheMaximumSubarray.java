1f2981ece0e5b866892460d21005b4b0a547787d
Kanahaiya Gupta <kanhaiyagupta20@gmail.com>
2018-05-20 14:25:56 +0530

    Added more solutions inside Practice>Algorithms category

---
+++ HackerRankDashboard/CoreCS/Algorithms/src/main/java/com/javaaid/hackerrank/solutions/algorithms/dynamicprogramming/TheMaximumSubarray.java
@@ -0,0 +1,40 @@
+package com.javaaid.hackerrank.solutions.algorithms.dynamicprogramming;
+
+import java.util.Scanner;
+
+public class TheMaximumSubarray {
+
+	static int[] maxSubarray(int[] arr) {
+		int max_so_far = Integer.MIN_VALUE, max_end_here = 0, maxSum = 0;
+		int ans[] = new int[2];
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > 0)
+				maxSum += arr[i];
+			max_end_here += arr[i];
+			max_end_here = Math.max(max_end_here, arr[i]);
+			max_so_far = Math.max(max_so_far, max_end_here);
+		}
+		ans[0] = max_so_far;
+		ans[1] = max_so_far < 0 ? max_so_far : maxSum;
+		return ans;
+
+	}
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for (int a0 = 0; a0 < t; a0++) {
+			int n = in.nextInt();
+			int[] arr = new int[n];
+			for (int arr_i = 0; arr_i < n; arr_i++) {
+				arr[arr_i] = in.nextInt();
+			}
+			int[] result = maxSubarray(arr);
+			for (int i = 0; i < result.length; i++) {
+				System.out.print(result[i] + (i != result.length - 1 ? " " : ""));
+			}
+			System.out.println("");
+
+		}
+		in.close();
+	}
+}
---
+++ HackerRankDashboard/CoreCS/Algorithms/src/main/java/com/javaaid/hackerrank/solutions/algorithms/recursion/RecursiveDigitSum.java
@@ -0,0 +1,45 @@
+package com.javaaid.hackerrank.solutions.algorithms.recursion;
+
+import java.math.BigInteger;
+
+public class RecursiveDigitSum {
+
+	// method 1 -> recursive solution
+	static int digitSum(String n, int k) {
+		long superDigit = getSuperDigit(n);
+		long n1 = getSuperDigit(superDigit);
+		long k1 = getSuperDigit(k);
+
+		long result = n1 * k1;
+
+		while (result / 10 != 0) {
+			result = getSuperDigit(result);
+		}
+
+		return (int) result;
+	}
+
+	private static long getSuperDigit(String n) {
+		if (n == null || n.isEmpty())
+			return 0;
+		return (n.charAt(0) - '0') + getSuperDigit(n.substring(1));
+	}
+
+	private static long getSuperDigit(long n) {
+		if (n / 10 != 0)
+			return (n % 10) + getSuperDigit(n / 10);
+		return (n % 10);
+	}
+
+	// method 2 > Iterative solutions hint -->http://applet-magic.com/digitsummod9.htm
+	static int digitSumUsingMathTrick(String n, int k) {
+		BigInteger n1 = new BigInteger(n);
+		n1 = n1.multiply(new BigInteger(k + ""));
+		n1 = n1.remainder(new BigInteger("9"));
+		return n1.intValue() == 0 ? 9 : n1.intValue();
+
+	}
+	
+	
+
+}
