7205f866a5385ce86bb114675e873471a35eca81
Kanahaiya Gupta <kanhaiyagupta20@gmail.com>
2018-11-01 13:43:32 +0530

    Added solution for ClassVsInstance problem

--- HackerRankDashboard/Tutorials/30DaysOfCode/src/main/java/com/javaaid/hackerrank/solutions/thirtydaysofcode/Day4ClassVsInstance.java
+++ HackerRankDashboard/Tutorials/30DaysOfCode/src/main/java/com/javaaid/hackerrank/solutions/thirtydaysofcode/Day4ClassVsInstance.java
@@ -7,6 +7,31 @@ package com.javaaid.hackerrank.solutions.thirtydaysofcode;
  * @author Kanahaiya Gupta
  *
  */
-public class Day4ClassVsInstance {
+public class Person {
+    private int age;	
+  
+	public Person(int initialAge) {
+        if(initialAge<0){
+            System.out.println("Age is not valid, setting age to 0.");
+        }
+        this.age=initialAge;
+  		// Add some more code to run some checks on initialAge
+	}
 
-}
+	public void amIOld() {
+  		// Write code determining if this person's age is old and print the correct statement:
+        if(age<13){
+            System.out.println("You are young.");
+        }else if(age>=13 && age<18){
+               System.out.println("You are a teenager.");
+
+        }else{
+            System.out.println("You are old.");
+        }            
+
+	}
+
+	public void yearPasses() {
+  		// Increment this person's age.
+        this.age++;
+	}
