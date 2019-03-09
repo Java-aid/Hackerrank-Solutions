b1e25a4b26c226bfca62d9d18a6cbd11bff42fdf
Kanahaiya Gupta <kanhaiyagupta20@gmail.com>
2019-03-09 07:49:26 +0530

    Updated solution with new hackerrank template

--- HackerRankDashboard/CoreCS/GeneralProgramming/src/main/java/com/javaaid/hackerrank/solutions/generalprogramming/basicprogramming/DrawingBook.java
+++ HackerRankDashboard/CoreCS/GeneralProgramming/src/main/java/com/javaaid/hackerrank/solutions/generalprogramming/basicprogramming/DrawingBook.java
@@ -13,23 +13,22 @@ import java.util.Scanner;
  *
  */
 public class DrawingBook {
+
+	static int pageCount(int n, int p) {
+
+		int totalPageTurnCountFromFront = n / 2;
+		int targetPageTurnCountFromFront = p / 2;
+		int targetPageTurnCountFromBack = totalPageTurnCountFromFront - targetPageTurnCountFromFront;
+
+		return Math.min(targetPageTurnCountFromFront, targetPageTurnCountFromBack);
+
+	}
+
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
 		int p = in.nextInt();
-		int result = 0;
-
-		if (2 * p <= n) {
-			result = p / 2;
-		} else {
-			if ((n & 1) == 0) {
-				result = (((p & 1) == 0) ? (n - p) / 2 : (n - p) / 2 + 1);
-
-			} else {
-				result = (n - p) / 2;
-			}
-		}
-		System.out.println(result);
+		System.out.println(pageCount(n, p));
 		in.close();
 	}
 }
