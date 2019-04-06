57d2f20b01e293e4f0042bbaec639c3c68dbed7b
Kanahaiya Gupta <kanhaiyagupta20@gmail.com>
2019-04-07 02:14:23 +0530

    Updated solution with latest template

--- HackerRankDashboard/CoreCS/DataStructures/src/main/java/com/javaaid/hackerrank/solutions/datastructures/arrays/ArrayManipulation.java
+++ HackerRankDashboard/CoreCS/DataStructures/src/main/java/com/javaaid/hackerrank/solutions/datastructures/arrays/ArrayManipulation.java
@@ -2,7 +2,7 @@
  * 
  * Problem Statement-
  * [Array Manipulation](https://www.hackerrank.com/challenges/crush/problem)    
- * 
+ * [Tutorial](https://youtu.be/hDhf04AJIRs)
  */
 package com.javaaid.hackerrank.solutions.datastructures.arrays;
 
@@ -16,13 +16,13 @@ public class ArrayManipulation {
 
 	static long arrayManipulation(int n, int[][] queries) {
 
-		long outputArray[] = new long[n + 1];
+		long outputArray[] = new long[n + 2];
 		for (int i = 0; i < queries.length; i++) {
 			int a = queries[i][0];
 			int b = queries[i][1];
 			int k = queries[i][2];
-			outputArray[a - 1] += k;
-			outputArray[b] -= k;
+			outputArray[a] += k;
+			outputArray[b+1] -= k;
 		}
 		long max = getMax(outputArray);
 		return max;
