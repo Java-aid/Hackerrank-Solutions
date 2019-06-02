77380af9e4d3a8e02cb1c9f566683889c3cfe258
Kanahaiya Gupta <kanhaiyagupta20@gmail.com>
2019-06-02 15:25:51 +0530

    Updated solution with latest hackerrank template

--- HackerRankDashboard/Tutorials/CrackingTheCodingInterview/src/main/java/com/javaaid/hackerrank/solutions/tutorials/ctci/LeftRotation.java
+++ HackerRankDashboard/Tutorials/CrackingTheCodingInterview/src/main/java/com/javaaid/hackerrank/solutions/tutorials/ctci/LeftRotation.java
@@ -2,10 +2,11 @@
  * 
  * Problem Statement-
  * [Arrays: Left Rotation](https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem)
- * 
+ * [Tutorial](https://youtu.be/TurKRD6Ne6w)
  */
 package com.javaaid.hackerrank.solutions.tutorials.ctci;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /**
@@ -13,37 +14,33 @@ import java.util.Scanner;
  *
  */
 public class LeftRotation {
+
+	static int[] rotLeft(int[] a, int d) {
+		int n = a.length;
+		int[] rotArray = new int[n];
+
+		for (int oldIndex = 0; oldIndex < n; oldIndex++) {
+			int newIndex = (oldIndex + d) % n;
+			rotArray[newIndex] = a[oldIndex];
+		}
+
+		return rotArray;
+
+	}
+
 	public static void main(String[] args) {
-		/*
-		 * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
-		 * class should be named Solution.
-		 */
 
 		Scanner sc = new Scanner(System.in);
 		int arraySize = sc.nextInt();
-		int key = sc.nextInt();
+		int d = sc.nextInt();
 		int a[] = new int[arraySize];
 		for (int i = 0; i < a.length; i++) {
 			a[i] = sc.nextInt();
 		}
 
-		leftRotate(a, 0, key - 1);
-		leftRotate(a, key, arraySize - 1);
-		leftRotate(a, 0, arraySize - 1);
-
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + " ");
-		}
+		int res[] = rotLeft(a, d);
+		System.out.println(Arrays.toString(res));
 		sc.close();
 	}
 
-	static void leftRotate(int a[], int start, int end) {
-		while (start < end) {
-			int temp = a[start];
-			a[start++] = a[end];
-			a[end--] = temp;
-
-		}
-
-	}
 }
