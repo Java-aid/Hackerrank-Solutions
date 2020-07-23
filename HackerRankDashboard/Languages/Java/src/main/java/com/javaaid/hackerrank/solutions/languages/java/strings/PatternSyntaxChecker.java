e93c50e3a6b55663e6753dc103c83b27d0eee40b
Abhinav Dhiman <abhinavg916@gmail.com>
2020-07-23 10:43:28 +0530

    Update PatternSyntaxChecker.java
    Corrected while loop Condition (testcases-- > 0)

--- HackerRankDashboard/Languages/Java/src/main/java/com/javaaid/hackerrank/solutions/languages/java/strings/PatternSyntaxChecker.java
+++ HackerRankDashboard/Languages/Java/src/main/java/com/javaaid/hackerrank/solutions/languages/java/strings/PatternSyntaxChecker.java
@@ -14,7 +14,7 @@ public class PatternSyntaxChecker {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		int testCases = Integer.parseInt(in.nextLine());
-		while (testCases > 0) {
+		while (testCases-- > 0) {
 			String pattern = in.nextLine();
 			try {
 				Pattern.compile(pattern);
