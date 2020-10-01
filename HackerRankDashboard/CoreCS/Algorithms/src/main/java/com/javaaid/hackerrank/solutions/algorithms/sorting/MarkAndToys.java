c31f6f59fc5a8cfd36e1c5671d87be6227c60091
AfreenKhan777 <71901755+AfreenKhan777@users.noreply.github.com>
2020-10-02 04:21:45 +0530

    Update MarkAndToys.java

--- HackerRankDashboard/CoreCS/Algorithms/src/main/java/com/javaaid/hackerrank/solutions/algorithms/sorting/MarkAndToys.java
+++ HackerRankDashboard/CoreCS/Algorithms/src/main/java/com/javaaid/hackerrank/solutions/algorithms/sorting/MarkAndToys.java
@@ -8,31 +8,46 @@ package com.javaaid.hackerrank.solutions.algorithms.sorting;
 
 import java.util.Arrays;
 import java.util.Scanner;
-
+import java.io.*;
 /**
  * @author Kanahaiya Gupta
  *
  */
-public class MarkAndToys {
-	public static void main(String[] args) {
+public class MarkAndToys 
+{
+	public static void main(String[] args)throws IOException
+	{
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-		int K = in.nextInt();
-		int a[] = new int[n];
-		for (int i = 0; i < n; i++) {
-			a[i] = in.nextInt();
+		int k = in.nextInt();
+		int prices[] = new int[n];
+		int i,j;
+		for (i = 0; i < n; i++)
+		{	
+			prices[i] = in.nextInt();
 		}
-		Arrays.sort(a);
-		int toyCount = 0, sum = 0;
-		for (int i = 0; i < a.length; i++) {
-			sum += a[i];
-			if (sum <= K) {
+		j=maximumToys(prices,k);
+		System.out.println(j);
+	}
+	public static int maximumToys(int[] prices,int k)
+	{
+		int n=prices.length;
+		Arrays.sort(prices);
+		//main programming
+    		int toyCount,i,sum;
+		sum=toyCount= 0;
+		for (i = 0; i < n; i++) 
+		{
+			sum += prices[i];
+			if (sum <= k) 
+			{
 				toyCount++;
-			} else {
+			} 
+			else 
+			{
 				break;
 			}
 		}
-		System.out.println(toyCount);
-		in.close();
+		return toyCount;
 	}
 }
