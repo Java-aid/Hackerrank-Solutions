ddf83fc2dbf7b76671360df9c5c9fb6703a48440
Richa <richakumari1508@gmail.com>
2020-10-03 16:47:36 +0530

    Java Anagrams program updated

--- HackerRankDashboard/Languages/Java/src/main/java/com/javaaid/hackerrank/solutions/languages/java/strings/JavaAnagrams.java
+++ HackerRankDashboard/Languages/Java/src/main/java/com/javaaid/hackerrank/solutions/languages/java/strings/JavaAnagrams.java
@@ -12,20 +12,17 @@ import java.util.Scanner;
 public class JavaAnagrams {
 
 	static boolean isAnagram(String a, String b) {
-		if (a.length() != b.length()) {
-			return false;
-		} else {
-			for (int i = 0; i < a.length(); i++) {
-				char ch = a.toLowerCase().charAt(i);
-				b = b.toLowerCase();
-				if (b.indexOf(ch) != -1) {
-					b = b.replaceFirst(ch + "", "");
-				} else {
-					return false;
-				}
-			}
-			return b.length() == 0;
-		}
+		 if(a.length()!=b.length())
+	            return false;
+	        int count[]=new int[26];
+	        for(int i=0;i<a.length();i++){
+	        count[a.charAt(i)-'a']++;
+	             count[b.charAt(i)-'a']--;
+	    }
+	        for(int c:count)
+	        if(c!=0)
+	            return false;
+	        return true;
 	}
 
 	public static void main(String[] args) {
