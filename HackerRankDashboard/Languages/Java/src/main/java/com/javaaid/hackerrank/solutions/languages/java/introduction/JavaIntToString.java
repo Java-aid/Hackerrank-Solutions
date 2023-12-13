7c4251d04e45232570cddfb1b7d549ea501fbde7
uday mishra <135223377+udaykod123@users.noreply.github.com>
2023-12-13 20:22:14 +0530

    Update JavaIntToString.java

--- HackerRankDashboard/Languages/Java/src/main/java/com/javaaid/hackerrank/solutions/languages/java/introduction/JavaIntToString.java
+++ HackerRankDashboard/Languages/Java/src/main/java/com/javaaid/hackerrank/solutions/languages/java/introduction/JavaIntToString.java
@@ -3,11 +3,52 @@
  */
 package com.javaaid.hackerrank.solutions.languages.java.introduction;
 
-/**
- * @author Kanahaiya Gupta
- *
- */
 public class JavaIntToString {
-	int n; //already decalared
+	int n; //already decalaredimport java.util.*;
+import java.security.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        DoNotTerminate.forbidExit();
+
+        try {
+            Scanner in = new Scanner(System.in);
+            int n = in.nextInt();
+            in.close();
+            String s = String.valueOf(n); // Convert integer n to string s
+
+            if (n == Integer.parseInt(s)) {
+                System.out.println("Good job");
+            } else {
+                System.out.println("Wrong answer.");
+            }
+        } catch (DoNotTerminate.ExitTrappedException e) {
+            System.out.println("Unsuccessful Termination!!");
+        }
+    }
+}
+
+// The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+    public static class ExitTrappedException extends SecurityException {
+
+        private static final long serialVersionUID = 1;
+    }
+
+    public static void forbidExit() {
+        final SecurityManager securityManager = new SecurityManager() {
+            @Override
+            public void checkPermission(Permission permission) {
+                if (permission.getName().contains("exitVM")) {
+                    throw new ExitTrappedException();
+                }
+            }
+        };
+        System.setSecurityManager(securityManager);
+    }
+}
+
 	String s=n+"";
 }
